4cb3d60524cc616351d994b25b4c538004afaa45
Farit Shamardanov <farit.shamardanov@outlook.com>
2020-01-01 03:30:24 +0300

    Add views and html

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Client, Product, Order
 
-# Register your models here.
+admin.site.register(Client)
+admin.site.register(Product)
+admin.site.register(Order)
---
+++ shop/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import basket, general_page, order, product_info
+
+urlpatterns = [
+    path('', general_page.index, name='general_page'),
+    path('basket/', basket.index, name='basket_page'),
+]
